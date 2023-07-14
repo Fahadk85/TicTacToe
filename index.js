1bb5c3b14c7024681775001f6e5715af608cc0c9
Fahadk85 <60100766+Fahadk85@users.noreply.github.com>
2023-07-14 15:06:20 -0400

    Added functionality to the restartGame function

--- index.js
+++ index.js
@@ -81,5 +81,9 @@ function checkWinner(){
 }
 
 function restartGame(){
-
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s turn`
+    cells.forEach(cell => cell.textContent = "");
+    running = true;
 }
